515526f0e61a2287106e8261a07f7bed1ca2f59b
Chermeux-Yann <108340872+Chermeux-Yann@users.noreply.github.com>
2022-09-07 13:32:33 +0200

    yann_footer

--- view/template.php
+++ view/template.php
@@ -43,13 +43,25 @@
 
         <div class="jumbotron jumbotron-fluid bg-dark">
             <div class="container">
-                <img src="public/imgs/dessins/card.jpg" alt="">
+                <img src="public/imgs/dessins/card.jpg" alt="" width=100%>
                 <h1 class="display-4 text-white text-center">LES MINIONS</h1>
             </div>
         </div>
     </header>
 
 <?php echo $content ?>
+
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2022 Copyright: Les MINIONS
+    
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
 
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
