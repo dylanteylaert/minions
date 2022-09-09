b8546784a02a9589b5cfe267833d23edfa7bb30a
Chermeux-Yann <108340872+Chermeux-Yann@users.noreply.github.com>
2022-09-09 09:05:57 +0200

    Add files via upload

--- models/model.php
+++ models/model.php
@@ -136,3 +136,65 @@ function updateDessins(){
     move_uploaded_file($_FILES['imgmodif']['tmp_name'], $fileName);
     
 }
+
+
+// create auteurs
+if (isset($_POST['submitAuteurs'])){
+    $db = dbConnect();
+    $sql ='INSERT INTO `auteurs`(`nom`, `style`, `photo`) VALUES (:nom,:style,:photo)';
+    $preparer = $db->prepare($sql);
+    $preparer->execute(
+    [
+        'nom' => $_POST['auteur'],
+        'style' => $_POST['style'],
+        'photo' => 'public/imgs/auteurs/' . $_FILES["imgauteur"]["name"]
+    ]
+    );
+    $target_dir = "public/imgs/dessins/";//lieu où sont enregistré les image 
+    $target_file = $target_dir . basename($_FILES["imgauteur"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+    // Vérification si le fichier est une image
+    if(isset($_POST["submitAuteurs"])) {
+    $check = getimagesize($_FILES["imgauteur"]["tmp_name"]);
+    
+    if($check !== false) {
+        $uploadOk = 1;
+    } else {
+        echo "Le fichier n'est pas une image.";
+        $uploadOk = 0;
+    }
+    }
+
+    // vérification si le fichier existe déjà
+    if (file_exists($target_file)) {
+    echo "Le fichier existe déjà.";
+    $uploadOk = 0;
+    }
+
+    // Vérification de la taille du fichier
+    if ($_FILES["imgauteur"]["size"] > 500000) {
+    echo "La taille du fichier est trop importante.";
+    $uploadOk = 0;
+    }
+
+    // Autorisation seulement de certain format de fichier
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {
+    echo "Seulement les fichiers JPG, JPEG, PNG et GIF sont autorisés.";
+    $uploadOk = 0;
+    }
+
+    // Vérification si $upload n'est pas à 0 (envoie message d'erreur)
+    if ($uploadOk == 0) {
+    echo " Le fichier n'a pas été envoyé.";
+    // Si tout est ok, alors le fichier est uploadé dans le bon dossier
+    } else {
+    if (move_uploaded_file($_FILES["imgauteur"]["tmp_name"], $target_file)) {
+        echo print_r($target_file);
+        echo "Le fichier ". htmlspecialchars( basename( $_FILES["imgauteur"]["name"])). " a été envoyé.";
+    } else {
+        echo " Erreur lors de l'envoi du fichier";
+    }
+    }
+};
--- view/template.php
+++ view/template.php
@@ -30,7 +30,7 @@
                     <a type="button" class="cou btn " href="index.php?action=addanim">Ajouter un animé et dessin animé</a>
                 </li>
                 <li class="nav-item">
-                    <a type="button" class="cou btn " href="index.php?action=addauteur">Ajouter un auteur</a>
+                    <a type="button" class="cou btn " href="index.php?action=addAuteurs">Ajouter un auteur</a>
                 </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
@@ -43,6 +43,20 @@
     </header>
 
 <?php echo $content ?>
+
+<!-- Footer -->
+<footer >
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2022 Copyright: Les Minions
+  
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
 
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
